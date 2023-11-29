10da3d1724da9e0b6d6a65e858074889eb7b83fa
Saurabh Saneja <saurabh250183@gmail.com>
2023-11-29 14:53:38 +0530

    minor

--- android/app/src/main/java/com/viamagustestinterview/MainApplication.java
+++ android/app/src/main/java/com/viamagustestinterview/MainApplication.java
@@ -16,8 +16,8 @@ public class MainApplication extends Application implements ReactApplication {
       new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          // return BuildConfig.DEBUG;
-          return false;
+          return BuildConfig.DEBUG;
+          // return false;
         }
 
         @Override
